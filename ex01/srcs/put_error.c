415fe4a74a7170158747d657438818e5d3b68569
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-26 11:18:23 +0200

    ex01: Use stderr

--- ex01/srcs/put_error.c
+++ ex01/srcs/put_error.c
@@ -6,22 +6,31 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 14:40:02 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/26 09:58:31 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/26 11:17:21 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 
-void	ft_putstr(char *str);
+void	ft_putstr_err(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		write(2, &str[i++], 1);
+}
+
 
 int	put_error(char *filename, char *program_name)
 {
-	ft_putstr(program_name);
-	ft_putstr(": ");
-	ft_putstr(filename);
-	ft_putstr(": ");
-	ft_putstr(strerror(errno));
-	ft_putstr("\n");
+	ft_putstr_err(program_name);
+	ft_putstr_err(": ");
+	ft_putstr_err(filename);
+	ft_putstr_err(": ");
+	ft_putstr_err(strerror(errno));
+	ft_putstr_err("\n");
 	return (1);
 }
