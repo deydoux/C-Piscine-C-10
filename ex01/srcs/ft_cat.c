f3dee7654a1ea70fea86fb0ef23fbdb195dc0a74
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-26 08:56:15 +0200

    ex01: Optimize with print_file function based on read_fd

--- ex01/srcs/ft_cat.c
+++ ex01/srcs/ft_cat.c
@@ -6,13 +6,11 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:25:10 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/25 18:21:50 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/26 08:54:22 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-
-int	read_fd(int fd, char *filename);
+int	print_file(char *filename);
 
 int	main(int argc, char **argv)
 {
@@ -20,12 +18,12 @@ int	main(int argc, char **argv)
 	int	code;
 
 	if (--argc == 0)
-		return (read_fd(0, 0));
+		return (print_file("/dev/stdin"));
 	argv++;
 	i = -1;
 	code = 0;
 	while (++i < argc)
-		if (read_fd(open(argv[i], O_RDONLY), argv[i]))
+		if (print_file(argv[i]))
 			code = 1;
 	return (code);
 }
--- ex01/srcs/print_file.c
+++ ex01/srcs/print_file.c
@@ -1,36 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_fd.c                                          :+:      :+:    :+:   */
+/*   print_file.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 17:57:43 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/25 19:42:59 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/26 08:54:14 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include <unistd.h>
 
 int		put_error(char *filename);
 void	ft_putstr(char *str);
 
-int	read_fd(int fd, char *filename)
+int	print_file(char *filename)
 {
+	int		file;
 	int		size;
 	char	buffer;
 
-	if (fd == -1)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (put_error(filename));
-	size = read(fd, &buffer, 1);
+	size = read(file, &buffer, 1);
 	while (size)
 	{
 		if (size == -1)
 			return (put_error(filename));
 		write(1, &buffer, 1);
-		size = read(fd, &buffer, 1);
+		size = read(file, &buffer, 1);
 	}
-	size = close(fd);
+	size = close(file);
 	if (size == -1)
 		return (put_error(filename));
 	return (0);
